65762244ef8300f45854470e1c7d1a250baa6696
Mark Brown <mark.brown8790@gmail.com>
2019-02-24 15:57:15 -0600

    Finished solver using recursion to step forward until a puzzle that works returns

--- C++/Sudoku Solver/SudokuSolver.cc
+++ C++/Sudoku Solver/SudokuSolver.cc
@@ -10,31 +10,24 @@
 #include <math.h>
 #include "QueueLoader.cc"
 
-void solveIt(int** data);
+//#define UNASSIGNED 0;
+
 int getLength(int** data);
-int* getArrayOfNums(int** data, int col);
-
-class Boards{	
-	int copy[25][25];
-	
-	public:
-	Boards();
-	bool checkMutability(int row, int col);
-	void makeCopy(int ** data);
-	void fillRows(int** data);
-};
-
+bool findUnassignedLoc(int** data, int &row, int &col);
+bool solveIt(int** data);
+bool isSafe(int** data, int row, int col, int num);
+bool usedInCol(int** data, int col, int num);
+bool usedInRow(int** data, int row, int num);
+bool usedInBox(int** data, int boxRow, int boxCol, int num);
 
 int main(int argc, char *argv[])
 {
 	QueueLoader queue;
-	Boards copy;
 	
 	int** data;
 	
 	std::string rawData = "";
 	while(std::getline(std::cin, rawData)){
-		std::cout << rawData << std::endl;
 		queue.loadQueue(rawData);
 	}
 	
@@ -43,19 +36,24 @@ int main(int argc, char *argv[])
 	
 	while(data != NULL){
 		
-		std::cout << "Solving Puzzle now...\n";
-		copy.makeCopy(data);
-		
-		copy.fillRows(data);
+		std::cout << "\nSolving Puzzle now...\n";
 		
 		int count = getLength(data);
-		
-		for(int i = 0; i < count; ++i){
-			for(int j = 0; j < count; ++j)
-				std::cout << data[i][j] << " ";
+			
+		if(solveIt(data)){
+			for(int i = 0; i < count; ++i){
+				for(int j = 0; j < count; ++j){
+					std::cout << data[i][j] << " ";
 				
-			std::cout << std::endl;
-		}
+				}
+				
+				std::cout << std::endl;
+			}
+			
+			std::cout << "Solved Puzzle!\n";
+		}else
+			std::cout << "failed to solve puzzle... \n";		
+			
 		
 		queue.nextItem();
 		data = queue.useData();
@@ -64,73 +62,6 @@ int main(int argc, char *argv[])
 	
 	delete [] data;
 	return 0;
-}
-
-// constructor
-Boards::Boards(){
-	for(int i = 0; i < 25; ++i)
-		for(int j = 0; j < 25; ++j)
-			copy[i][j] = -1;
-}
-
-// Make copy of board (check immutability)
-void Boards::makeCopy(int** data){
-	for(int i = 0; i < 25; ++i)
-		for(int j = 0; j < 25; ++j){
-			if(data[i][j] == -1)
-				copy[i][j] = -2;
-			else
-				copy[i][j] = data[i][j];
-				
-		}
-}
-
-// find missing numbers
-int* getArrayOfNums(int** data, int row){
-	int count = getLength(data);
-	
-	int * temp = NULL;
-	
-	temp = new int[count];
-	
-	for(int i = 0; i < count; ++i)
-		temp[i] = i + 1;
-		
-	for(int i = 0; i < count; ++i)
-		for(int j = 0; j < count; ++j)
-			if(temp[i] == data[row][j]){
-				temp[i] = 0;
-			}
-	
-	return temp;
-}
-
-// fills in the rows with needed values
-void Boards::fillRows(int** data){
-	
-	int lth = getLength(data);			
-	int* tmp = NULL;
-	tmp = new int[lth];
-	int ticker = 0;
-	
-	for(int i = 0; i < lth; ++i){
-		for(int j = 0; j < lth; ++j){
-			
-			tmp = getArrayOfNums(data, i);
-			
-			if(copy[i][j] == 0){
-				
-				while(tmp[ticker] == 0 && ticker < lth) { ticker++; }
-				
-				data[i][j] = tmp[ticker];
-				ticker++;
-			}
-		}
-		ticker = 0;
-	}
-		
-		
-	delete [] tmp;
 }
 
 // returns to the count of the dimension
@@ -145,14 +76,74 @@ int getLength(int** data){
 	return count;
 }
 
-// calculates score of the position
-
-// returns bool if value can be changed
-bool Boards::checkMutability(int row, int col){
-	return (copy[row][col] != 0);
+// If this works make sure to review why it works!! 
+// finds the first instance of an unassigned value in the puzzle (by passing in the row, col by reference -> smart!)
+bool findUnassignedLoc(int** data, int &row, int &col){
+	int lth = getLength(data);
+	
+	for(row = 0; row < lth; ++row)
+		for(col = 0; col < lth; ++col)
+			if (data[row][col] == 0)
+				return true;
+	
+	return false;
 }
 
-// takes in the pointer and solves the puzzle
-void solveIt(int** data){
-	// 
+bool usedInCol(int** data, int col, int num){
+	int lth = getLength(data);
+	
+	for(int row = 0; row < lth; ++row)
+		if(data[row][col] == num)
+			return true;
+			
+	return false;
+}
+
+bool usedInRow(int** data, int row, int num){
+	int lth = getLength(data);
+	
+	for(int col = 0; col < lth; ++col)
+		if(data[row][col] == num)
+			return true;
+			
+	return false;
+}
+
+bool usedInBox(int** data, int boxRow, int boxCol, int num){
+	int lth = sqrt(getLength(data));	
+	
+	for(int row = 0; row < lth; ++row)
+		for(int col = 0; col < lth; ++col)
+			if(data[row+boxRow][col+boxCol] == num)
+				return true;
+				
+	return false;
+}
+
+// MAKE SURE TO EXPLAIN THIS ONE IF IT WORKS!!!!
+bool isSafe(int** data, int row, int col, int num){
+	int sqrtLth = sqrt(getLength(data));
+	
+	return !usedInRow(data, row, num) && !usedInCol(data, col, num) && !usedInBox(data, (row - row%sqrtLth), (col - col%sqrtLth) , num) && data[row][col] == 0;
+}
+
+// Solve it function
+bool solveIt(int** data){
+	
+	int row, col;
+	int num = getLength(data);
+	
+	if(!findUnassignedLoc(data, row, col))
+		return true; // success!!
+		
+	for(int i = 1; i <= num; ++i)
+		if(isSafe(data, row, col, i)){
+			data[row][col] = i;
+			if(solveIt(data))
+				return true;
+				
+			data[row][col] = 0;
+		}
+	
+	return false;
 }
