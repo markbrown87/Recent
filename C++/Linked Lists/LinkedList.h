8201af92b3a3c34f0c2eaf00e0cc8a43d15528f1
mark.a.brown <mark.a.brown@accenture.com>
2019-02-26 11:19:18 -0600

    Finished basic implementation of many linked list features

--- C++/Linked Lists/LinkedList.h
+++ C++/Linked Lists/LinkedList.h
@@ -34,7 +34,7 @@ template <class type>
 class LinkedList{
 	Node<type>* current;
 	Node<type>* head;
-	void destoryList();
+	void destoryList(Node<type>* ticker);
 	
 	
 	public:
@@ -46,7 +46,7 @@ class LinkedList{
 	
 };
 
-// INSERT DESCRIPTIONS
+// Inserts data into the list
 template <class type>
 void LinkedList<type>::insert(type data){
 	Node<type>* temp;
@@ -66,17 +66,46 @@ void LinkedList<type>::insert(type data){
 		std::cout << "Error inserting data into list -> please resolve...\n";
 }
 
+// Constructor/Destructor
+template <class type>
+LinkedList<type>::LinkedList(){
+	Node<type>* tmp;
+	tmp = new Node<type>;
+	
+	tmp -> next = NULL;
+	tmp -> data = NULL;
+	
+	head = tmp;
+	current = head;
+}
+template <class type>
+LinkedList<type>::~LinkedList(){
+	destoryList(head);
+}
+
+// returns the head of the list
 template <class type>
 Node<type>* LinkedList<type>::rBegin(){ return head; }
 
+// overwrites the data in the 'current' node
 template <class type>
-void LinkedList<type>::assign(type data){
-	// CHANGE THE DATA HERE
-}
+void LinkedList<type>::assign(type data){ current -> data = data; }
 
+// destroys the list
 template <class type>
-void LinkedList<type>::destoryList(){
+void LinkedList<type>::destoryList(Node<type>* ticker){
 	// destroy the list by removing either head to tail in recursive fashion (make sure its reverse recursion)
+	Node<type>* troy;
+	troy = ticker -> next;
+
+	if(troy == NULL){
+		std::cout << "List Destroyed...\n";
+		return; // Destroyed!!
+	} else {
+		delete ticker;
+		
+		destoryList(troy);
+	}
 }
 
 #endif
