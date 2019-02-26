c897ec55961ef6f97c580cf945b21f02a7507d9e
mark.a.brown <mark.a.brown@accenture.com>
2019-02-26 10:48:33 -0600

    Linked List builds successfully; implementation next

--- C++/Linked Lists/LinkedList.h
+++ C++/Linked Lists/LinkedList.h
@@ -6,28 +6,32 @@
 #include <iostream>
 #include <string>
 
+#ifndef __LINKEDLISTS_H__
+#define __LINKEDLISTS_H__
+
 template <class type>
-struct Node<type>{
+struct Node{
 	Node();
 	~Node();
 	type* next;
 	type* data;
 };
 
-Node::Node(){
+template <class type>
+Node<type>::Node(){
 	next = NULL;
 	data = NULL;
 }
 
-Node::~Node(){
+template <class type>
+Node<type>::~Node(){
 	delete next;
-	delete prev;
 	delete data;
 }
 
 
 template <class type>
-class LinkedList<type>{
+class LinkedList{
 	Node<type>* current;
 	Node<type>* head;
 	void destoryList();
@@ -44,18 +48,21 @@ class LinkedList<type>{
 
 // INSERT DESCRIPTIONS
 template <class type>
-void insert(type data){
+void LinkedList<type>::insert(type data){
 	// INSERT DATA
 }
 
 template <class type>
-Node<type>* rBegin(){ return head; }
+Node<type>* LinkedList<type>::rBegin(){ return head; }
 
 template <class type>
-void assign(type data){
+void LinkedList<type>::assign(type data){
 	// CHANGE THE DATA HERE
 }
 
-void destoryList(){
+template <class type>
+void LinkedList<type>::destoryList(){
 	// destroy the list by removing either head to tail in recursive fashion (make sure its reverse recursion)
 }
+
+#endif
